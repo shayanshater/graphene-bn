e6f481f5cb4a4c3a57aaf1c1e136947371bd53b8
shayan <shayanshaterpoori@outlook.com>
2025-10-21 19:58:45 +0100

    restore basic images

--- main.py
+++ main.py
@@ -17,8 +17,8 @@ def epsilon(max_kx, max_ky, a, t):
         elev: elevation angle for 3D view
         azim: azimuth angle for 3D view
     """
-    kx = np.linspace(-max_kx, max_kx, 100)
-    ky = np.linspace(-max_ky, max_ky, 100)
+    kx = np.linspace(-max_kx, max_kx, 1000)
+    ky = np.linspace(-max_ky, max_ky, 1000)
     KX, KY = np.meshgrid(kx, ky)
     epsilon_p = t * np.sqrt(
         1 + 
